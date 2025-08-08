b25a61929efc91e43e2bcbbc7809a67c12786c25
Rouven <anderer.rouven@web.de>
2025-08-08 23:31:42 +0200

    feat: add h1 support

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@ export default class SummaryCanvasGeneratorPlugin extends Plugin {
 		await this.loadSettings();
 
 		this.addRibbonIcon('dice', 'Greet', async () => {
-			const fileContainsFilter = 'SPL'
+			const fileContainsFilter = 'V1 SPL'
 
 			// Get the file using the path
 			const files = this.app.vault.getMarkdownFiles()
@@ -110,38 +110,68 @@ export default class SummaryCanvasGeneratorPlugin extends Plugin {
 			} else {
 			  console.log(`File not found: ${file?.name}`);
 			}
+			const DEFAULT_DISTANCE_BETWEEN_NODES = 50;
+			const DEFAULT_LINE_HEIGHT = 30;
+			const DEFAULT_IMAGE_HEIGHT = 250;
+			const DEFAULT_DISTANCE_BETWEEN_COLUMNS = 600;
+			const DEFAULT_COLUMN_WIDTH = 500;
+			const H2_X_OFFSET = 25;
 
-			const firstH2SectionPosition = fileContent.search("\n## ");
-			console.log(firstH2SectionPosition);
+			const h1Sections = fileContent.split(/\n[#]{1,1} /gm).filter(s => !!s);
+			console.log(h1Sections);
+
+			const canvas = (this.app.workspace.getLeavesOfType('canvas')[0].view as any).canvas;
+			console.log(canvas);
 			
-			if (firstH2SectionPosition > 0) {
-				const h2Sections = fileContent.substring(firstH2SectionPosition).split('\n## ').filter(s => !!s);
+			let previousY = 0;
+			let previousHeight = 0;
+			for (let i = 1; i < h1Sections.length; i++) { // i = 1 to skip part before first header
+				const h1Section = h1Sections[i];
+				const firstH2Position = h1Section.search(/[#]{2,2} /gm);
+				console.log(firstH2Position);
+				
+				const h1Text = h1Section.substring(0, firstH2Position);
+				console.log(h1Text);
+				
+
+				if (h1Text.length > 0) {
+					const lines = h1Text.split('\n').length;
+					const numberOfImages = h1Text.split('\n').filter((line) => line.contains('.png') || line.contains('.jpg')).length;
+					const newHeight = lines * DEFAULT_LINE_HEIGHT + numberOfImages * DEFAULT_IMAGE_HEIGHT
+					const newY = previousY + previousHeight + DEFAULT_DISTANCE_BETWEEN_NODES;
+					canvas.createTextNode({
+						pos: { x: positionIndex * DEFAULT_DISTANCE_BETWEEN_COLUMNS, y: newY },
+						text: '# ' + h1Text,
+						size: { width: DEFAULT_COLUMN_WIDTH, height: newHeight },
+						focus: false,
+					});
+					
+					previousHeight = newHeight;
+					previousY = newY;
+				}
+
+				const h2Sections = h1Section.split(/[#]{2,2} /gm)
 				console.log(h2Sections);
 				
-	
-				const canvas = (this.app.workspace.getLeavesOfType('canvas')[0].view as any).canvas;
-				console.log(canvas);
-				
-				let previousY = 0;
-				let previousHeight = 0;
-				const distanceBetweenNodes = 50;
-				const lineHeight = 30;
-				for (let i = 0; i < h2Sections.length; i++) {
-					const element = h2Sections[i];
-					const lines = element.split('\n').length;
-					const numberOfImages = element.split('\n').filter((line) => line.contains('.png') || line.contains('.jpg')).length;
-					const newHeight = lines * lineHeight + numberOfImages * 250
-					const newY = previousY + previousHeight + distanceBetweenNodes;
+
+				for (let j = 1; j < h2Sections.length; j++) {
+					const h2Section = h2Sections[j];
+
+					const lines = h2Section.split('\n').length;
+					const numberOfImages = h2Section.split('\n').filter((line) => line.contains('.png') || line.contains('.jpg')).length;
+					const newHeight = lines * DEFAULT_LINE_HEIGHT + numberOfImages * DEFAULT_IMAGE_HEIGHT
+					const newY = previousY + previousHeight + DEFAULT_DISTANCE_BETWEEN_NODES;
 					const lineNode = canvas.createTextNode({
-						pos: { x: positionIndex * 600, y: newY },
-						text: `![[${file?.name.replace('.md', '')}#${element.split('\n')[0]}]]`.replace(' #c', ' c'),
-						size: { width: 500, height: newHeight },
+						pos: { x: positionIndex * DEFAULT_DISTANCE_BETWEEN_COLUMNS + H2_X_OFFSET, y: newY },
+						text: `![[${file?.name.replace('.md', '')}#${h2Section.split('\n')[0]}]]`.replace(' #c', ' c'),
+						size: { width: DEFAULT_COLUMN_WIDTH, height: newHeight },
 						focus: false,
 					});
 					console.log(lineNode);
 					
 					previousHeight = newHeight;
 					previousY = newY;
+					
 				}
 			}
 			
